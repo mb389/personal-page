d53adb088408876f6cfa224989879e28f1088e3d
Michael Bushoy <mbushoy@gmail.com>
2018-07-05 21:22:42 -0400

    tweaks

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,43 +29,49 @@ class Home extends Component {
 				<h2>Technologies</h2>
 
 				<div className="skill-item clearfix">
-					<h4>Client Side JS</h4>
+					<h4>Client Side</h4>
 					<ul>
 						<li><em>Angular</em></li>
 						<li><em><b>*</b>React</em></li>
 							<ul>
 								<li><em><b>*</b>Redux</em></li>
 							</ul>
-						<li><em>D3</em></li>
-            <li><em>socket.io</em></li>
+            <li><em><b>*</b>CSS / SASS</em></li>
 					</ul>
 				</div>
 
 				<div className="skill-item clearfix">
-					<h4>Server Side JS</h4>
+					<h4>Server Side</h4>
 					<ul>
 						<li><em><b>*</b>Node</em></li>
-							<ul>
-								<li><em><b>*</b>Express</em></li>
-							</ul>
-              <li><em>MongoDB</em></li>
-              <ul>
-                <li><em>Mongoose</em></li>
-              </ul>
-              <li><em>SQL</em></li>
-                <ul>
-                  <li><em>Sequelize</em></li>
-                </ul>
+						<ul>
+							<li><em><b>*</b>Express</em></li>
+						</ul>
+            <li><em>Python</em></li>
+            <ul>
+              <li><em>Flask</em></li>
+            </ul>
+            <li><em>Go</em></li>
+            <li><em>MongoDB</em></li>
+            <ul>
+              <li><em>Mongoose</em></li>
+              <li><em>mgo</em></li>
+            </ul>
+            <li><em>SQL</em></li>
+            <ul>
+              <li><em>Sequelize</em></li>
+              <li><em>SQL Alchemy</em></li>
+            </ul>
 					</ul>
 				</div>
 
 				<div className="skill-item clearfix">
 					<h4>Testing</h4>
 					<ul>
-						<li><em>Jasmine</em></li>
-						<li><em>Karma</em></li>
+						<li><em>Jest</em></li>
 						<li><em>Mocha</em></li>
 						<li><em>Chai</em></li>
+            <li><em>PyTest</em></li>
 					</ul>
 				</div>
 
@@ -77,6 +83,17 @@ class Home extends Component {
 						<li><em>Heroku</em></li>
 					</ul>
 				</div>
+
+        <div className="skill-item clearfix">
+          <h4>Infrastructure</h4>
+          <ul className="clearfix">
+            <li><em>Docker</em></li>
+            <li><em>CircleCI</em></li>
+            <li><em>Nginx</em></li>
+            <li><em>AWS</em></li>
+            <li><em>GCS</em></li>
+          </ul>
+        </div>
 
 				<div className="exclaimation">
 					<em><b>*</b> This site is built with these technologies. View the <a href="https://github.com/mb389/portfolio-react-redux">github repo here</a>.</em><br/>
