ad3b422269e571c6c553b22491ffe63e2d47062b
Michael Bushoy <mbushoy@gmail.com>
2019-09-27 16:10:49 -0400

    update configs

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,22 +1,22 @@
-import webpack from 'webpack';
-import path from 'path';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import webpack from "webpack";
+import path from "path";
+import ExtractTextPlugin from "extract-text-webpack-plugin";
 
 const GLOBALS = {
-  'process.env.NODE_ENV': JSON.stringify('production'),
+  "process.env.NODE_ENV": JSON.stringify("production"),
   __DEV__: false
 };
 
 export default {
   debug: true,
-  devtool: 'source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
+  devtool: "source-map", // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
   noInfo: true, // set to false to see a list of every file being bundled.
-  entry: './src/index',
-  target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
+  entry: "./src/index",
+  target: "web", // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
-    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: __dirname + "/dist", // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: "/",
+    filename: "bundle.js"
   },
   plugins: [
     new webpack.DefinePlugin(GLOBALS), //Tells React to build in prod mode. https://facebook.github.io/react/downloads.html
@@ -25,21 +25,40 @@ export default {
     new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
-          loaders:
-          [
-            {test: /\.js?$/, loader: 'babel-loader?cacheDirectory', exclude: /(node_modules|bower_components)/ },
-            {test: /\.jsx?$/, loader: 'babel-loader?cacheDirectory', exclude: /(node_modules|bower_components)/ },
-            {test: /(\.css|\.scss)$/, loaders: ['style', 'css', 'sass']},
-            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=image/svg+xml'},
-            {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/font-woff"},
-            {test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/font-woff"},
-            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/octet-stream"},
-            {test: /\.ico$/, loader: 'file-loader?name=[name].[ext]'},
-            {test: /\.(jpe?g|png|gif)$/i, loaders: ['file']},
-            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader"},
-        ]
+    loaders: [
+      {
+        test: /\.js?$/,
+        loader: "babel-loader?cacheDirectory",
+        exclude: /(node_modules|bower_components)/
       },
-resolve: {
-    extensions: ['', '.js', '.jsx', '.css', '.scss', '.sass']
-}
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader?cacheDirectory",
+        exclude: /(node_modules|bower_components)/
+      },
+      { test: /\.css?$/, loaders: ["style", "css"] },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader?mimetype=image/svg+xml"
+      },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader?mimetype=application/font-woff"
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader?mimetype=application/font-woff"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader?mimetype=application/octet-stream"
+      },
+      { test: /\.ico$/, loader: "file-loader?name=[name].[ext]" },
+      { test: /\.(jpe?g|png|gif)$/i, loaders: ["file"] },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" }
+    ]
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx", ".css"]
+  }
 };
