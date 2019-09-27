de650474454965c667a9b4d3edd8f1fbbca08fd0
Michael Bushoy <mbushoy@gmail.com>
2019-09-27 18:01:02 -0400

    try port again

--- tools/expressServer.js
+++ tools/expressServer.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use("/", express.static("dist"));
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Listening at ", process.env.PORT);
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+  console.log("Listening at ", port);
 });
